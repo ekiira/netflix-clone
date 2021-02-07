4e3716c099b306c7b4b751e83af971c136880242
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-02-07 23:59:08 +0100

    app set-up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss";
 
+import Home from "./components/Home/Home";
+import Gallery from "./components/Gallery/Gallery";
+import FAQ from "./components/FAQ/Faq";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <Gallery />
+      <FAQ />
+      <Footer />
     </div>
   );
 }
