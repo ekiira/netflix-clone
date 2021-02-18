760bad8aad94adaafebd9abb40d5888d534be551
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-02-18 22:36:50 +0100

    footer-section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,49 @@
 import React from "react";
 
-export default function () {
-  return <div>footer</div>;
+import "./footer.scss";
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <p className="title">Questions? Contact us.</p>
+
+        <div className="row">
+          <div className="col">
+            <ul>
+              <li>FAQ</li>
+              <li>Investor Relations</li>
+              <li>Privacy</li>
+              <li>Speed Test</li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preerences</li>
+              <li>Legal Notices</li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Account</li>
+              <li>Ways to Watch</li>
+              <li>Corporate Information</li>
+              <li>Netflix Originals</li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Media Center</li>
+              <li>Terms of Use</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <p className="bottom">Netflix Nigeria</p>
+      </div>
+    </div>
+  );
 }
+
+export default Footer;
